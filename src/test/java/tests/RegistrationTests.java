9143cea06cb0d1d25104d4a34f0249a184b7b355
לב לאונטייב <152332597+ZLA964@users.noreply.github.com>
2024-11-26 15:05:43 +0300

    all as at midl of lesson

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -4,18 +4,11 @@ import manager.ApplicationManager;
 import org.testng.annotations.Test;
 import pages.HomePage;
 
-import static org.testng.Assert.assertTrue;
-
 public class RegistrationTests extends ApplicationManager {
 
     @Test
     void registrationPositiveTest(){
         new HomePage(getDriver());
-    }
-
-    @Test
-    void testRegistration() {
-        assertTrue(true);
     }
 
 }
